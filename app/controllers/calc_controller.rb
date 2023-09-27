ed8cb476374240b6042360bc7d94fd28a614ad9f
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-27 22:01:23 +0000

    finished payment pages and random_new

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -19,4 +19,29 @@ class CalcController < ApplicationController
     @the_result = @num ** 0.5
     render({:template => "calc_templates/square_root_results"})
   end
+
+  def payment_new
+    render(:template => "calc_templates/payment_new")
+  end
+
+  def payment_result
+    @apr = params.fetch("apr").to_f
+    @num_of_years = params.fetch("year").to_i
+
+    @principal = params.fetch("principal").to_i
+
+    periods_remaining = @num_of_years * 12
+    monthly_interest_rate = @apr.round(4) / 100 / 12
+    numerator = @principal * monthly_interest_rate * (1 + monthly_interest_rate)** periods_remaining
+    denominator = (1 + monthly_interest_rate)** periods_remaining - 1
+
+    @monthly_payment = numerator / denominator
+
+    @formatted_apr = sprintf("%.4f", @apr)
+    render(:template => "calc_templates/payment_results")
+  end
+
+  def random_new
+    render(:template =>"calc_templates/random_new")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get("/square/results", {:controller => "calc", :action => "square_result"})
   get("/square_root/new", {:controller => "calc", :action => "square_root_new"})
   get("/square_root/results", {:controller => "calc", :action => "square_root_result"})
+  get("/payment/new", {:controller => "calc", :action =>"payment_new"})
+  get("/payment/results", {:controller => "calc", :action => "payment_result"})
+  get("random/new", {:controller => "calc", :action => "random_new"})
 end
